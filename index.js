80683cae6a16ab67e1186ef8a971263485c52c9f
mdsohelrana888 <halalulislamsohel@gmail.com>
2025-10-07 02:57:54 +0600

    Use environment variables for Firebase credentials

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Initialize Firebase Admin SDK
-let serviceAccount;
-if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
-  const decodedString = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf-8');
-  serviceAccount = JSON.parse(decodedString);
-} else {
-  serviceAccount = require("./firebase-admin-sdk.json");
-}
+const decodedString = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf-8');
+const serviceAccount = JSON.parse(decodedString);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
